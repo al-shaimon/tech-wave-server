9a551c3ac2c22da8efcc9d641c67a2131b11d276
al-shaimon <alshaimon968@gmail.com>
2024-09-29 03:08:46 +0600

    build

---
+++ dist/app.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const http_status_1 = __importDefault(require("http-status"));
+const config_1 = __importDefault(require("./app/config"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    credentials: true,
+    origin: [config_1.default.client_url],
+}));
+app.use((0, cookie_parser_1.default)());
+//parser
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+//Testing
+app.get('/', (req, res, next) => {
+    res.status(http_status_1.default.OK).json({
+        success: true,
+        message: 'Welcome to the Tech Wave API',
+    });
+});
+exports.default = app;
